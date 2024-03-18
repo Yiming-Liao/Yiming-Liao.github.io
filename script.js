c0c98a560ec2a7c77763508f23e801335c7e1616
Yiming Liao <yimingliao.official@gmail.com>
2024-03-18 23:43:24 +0800

    INDEX

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*
+window.addEventListener('scroll', () => {
+    const scrolledHeight = window.scrollY;
+    const windowHeight = window.innerHeight;
+    // 當使用者下滑超過畫面一半時
+    if (scrolledHeight > 100) {
+      document.querySelector(".div-opening").style.opacity = "0.2"; // 隱藏歡迎畫面
+      document.querySelector(".div-opening").style.transform = 'translateY(100px)';
+    } else if (scrolledHeight <100) {
+        document.querySelector(".div-opening").style.opacity = "1";
+        document.querySelector(".div-opening").style.transform = 'translateY(0)';
+
+    }
+  });
+   */
